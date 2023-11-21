e249299bd30b9d6531569c54815030cd0e1680d9
Matt Sikes <emsikes@gmail.com>
2023-11-21 09:30:48 -0800

    testing deployment again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello, World
+          Hello, World!  Testing with Elastic Beanstalk....
         </p>
         <a
           className="App-link"
